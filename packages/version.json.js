b3055d5d3cb80e3e02244baecd5fd4e6c103d7ac
hq450 <qhuang450@gmail.com>
2024-12-10 10:28:13 +0800

    clean commit

---
+++ packages/version.json.js
@@ -0,0 +1,14 @@
+{
+  "name": "fancyss",
+  "version": "3.3.5",
+  "md5_hnd_full": "1f14f473cb39bfab128dcb73d6999eec",
+  "md5_hnd_lite": "57f65f453f9774250ec9ece7ca099770",
+  "md5_hnd_v8_full": "a9f4dcfd7ea46e87c829f2dc5b669964",
+  "md5_hnd_v8_lite": "6731a0cf87374fa740aaedffbe44b822",
+  "md5_qca_full": "44fea8940c54c18ed0582df1be1d7729",
+  "md5_qca_lite": "504e5df519ff95c503f86c113c9df59c",
+  "md5_arm_full": "13e9f8479ac90df1c26ade6a68b5e55f",
+  "md5_arm_lite": "4bce5acb358162d304cd0eed85b8325a",
+  "md5_mtk_full": "071d695e7ed193eb8db3c11d470f3d5e",
+  "md5_mtk_lite": "e5f0badc86bc5a3ac6063038b7b41f88"
+}
---
+++ rules/rules.json.js
@@ -0,0 +1,95 @@
+{
+  "gfwlist": {
+    "name": "gfwlist.conf",
+    "date": "2024-12-09 21:39",
+    "md5": "ef2b95b13aaf1b016ff762ef586da308",
+    "count": "6828"
+  },
+  "chnroute_maxmind": {
+    "name": "chnroute_maxmind.txt",
+    "date": "2024-09-29 11:26",
+    "md5": "41302df69ece45fe11d3e9ddcad2c47e",
+    "count": "7212",
+    "count_ip": "345532148",
+    "source": "maxmind/geolite2",
+    "url": "https://github.com/firehol/blocklist-ipsets/blob/master/geolite2_country/country_cn.netset"
+  },
+  "chnroute_ipip": {
+    "name": "chnroute_ipip.txt",
+    "date": "2024-11-01 00:38",
+    "md5": "4dc3943cb26f84c28cbdcbd46ff3399d",
+    "count": "7395",
+    "count_ip": "354951706",
+    "source": "ipip.net",
+    "url": "https://github.com/firehol/blocklist-ipsets/blob/master/ipip_country/ipip_country_cn.netset"
+  },
+  "chnroute_17mon": {
+    "name": "chnroute_17mon.txt",
+    "date": "2024-09-29 11:26",
+    "md5": "4e37080ae9dddb3349e5491cf6d137b0",
+    "count": "6885",
+    "count_ip": "354433392",
+    "source": "17mon/ipip",
+    "url": "https://raw.githubusercontent.com/17mon/china_ip_list/refs/heads/master/china_ip_list.txt"
+  },
+  "chnroute_misakaio": {
+    "name": "chnroute_misakaio.txt",
+    "date": "2024-12-09 21:39",
+    "md5": "d3aedec301edd8f54e0d0def80649132",
+    "count": "4201",
+    "count_ip": "285666048",
+    "source": "misakaio",
+    "url": "https://github.com/misakaio/chnroutes2/blob/master/chnroutes.txt"
+  },
+  "chnroute_cnisp": {
+    "name": "chnroute_cnisp.txt",
+    "date": "2024-09-29 11:26",
+    "md5": "4e37080ae9dddb3349e5491cf6d137b0",
+    "count": "6885",
+    "count_ip": "354433392",
+    "source": "cnisp",
+    "url": "https://github.com/gaoyifan/china-operator-ip"
+  },
+  "chnroute_apnic": {
+    "name": "chnroute_apnic.txt",
+    "date": "2024-12-09 21:39",
+    "md5": "2ddb60212a1582e76a20218c6c940d39",
+    "count": "8662",
+    "count_ip": "343140864",
+    "source": "apnic",
+    "url": "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
+  },
+  "chnroute": {
+    "name": "chnroute.txt",
+    "date": "2024-12-09 21:39",
+    "md5": "90652242536462c24df068c005f5121d",
+    "count": "8406",
+    "count_ip": "359385604",
+    "source": "fancyss",
+    "url": "https://github.com/hq450/fancyss/tree/3.0/rules"
+  },
+  "cdn_china": {
+    "name": "cdn.txt",
+    "date": "2024-12-09 21:39",
+    "md5": "36deefedeb032684f56c366646013b4b",
+    "count": "94075"
+  },
+  "apple_china": {
+    "name": "apple_china.txt",
+    "date": "2024-11-01 00:38",
+    "md5": "216ccd6e184055ba6133687a1c42aa3b",
+    "count": "174"
+  },
+  "google_china": {
+    "name": "google_china.txt",
+    "date": "2024-09-29 11:26",
+    "md5": "6899bb37a930b7b4c89528035c90f5f4",
+    "count": "191"
+  },
+  "cdn_test": {
+    "name": "cdn_test.txt",
+    "date": "2024-12-03 01:26",
+    "md5": "84b034525bca6b60fbffb68dc8fbb9cd",
+    "count": "87"
+  }
+}
